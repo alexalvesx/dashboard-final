ab6da3c5b2224b1657f744af2b68b77b9791609b
Alexsander <alex_alves.x@hotmail.com>
2026-04-24 21:23:29 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -30,6 +30,31 @@
         </main>
     </div>
     <?php include 'footer.php'; ?>
+
+    <script>
+    // 1.Pergunta ao usurário e guarda a resposta na variável
+    let categorias = prompt("Quantas categorias você deseja cadastrar?");
+    let nomeCategoria
+    
+
+    // 2. Converte o texto que o usuário digitou para um número real
+    let quantidade = Number(categorias);
+
+    // 3.Cria um laço de repetição que roda a quantidade de vezes informada
+    for (let i = 1; i <= quantidade; i++){
+        nomeCategoria = prompt("Digite o nome da categoria "+i);
+        // Mostra o progresso apenas no F12 (Console)
+        if (nomeCategoria == ""){
+            console.log("Erro: O nome da categoria não pode ser vazio!");
+        } else {
+            console.log("Categoria "+nomeCategoria+" cadastrada com sucesso!");
+        }
+    }
+
+    alert("Finalizado!");
+
+    </script>
+
 </body>
 
 </html>
